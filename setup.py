d918c360e1dc434387f8675ec3997501ad6a1a2c
Leo Aqua <88901298+Leo-Aqua@users.noreply.github.com>
2024-03-26 08:45:18 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from setuptools import setup, find_packages
+import subprocess
+import os
+
+remote_verion = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode('utf8').strip()
+assert "." in remote_verion
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="IServAPI",
-    version="0.1.0a",
+    version=remote_verion,
     author="Leo Aqua",
     author_email="contact@leoaqua.de",
     description="Unofficial API for IServ",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Leo-Aqua/IServAPI",
+    keywords=["IServ", "IServAPI", "iserv api", "iserv-api", "API", "Leo-Aqua"],
     packages=find_packages(),
     install_requires=[
         "requests",
