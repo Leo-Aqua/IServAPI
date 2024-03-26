3419ec335a4bfcae2c84b1bdfc67dd422bcfab1c
Leo Aqua <88901298+Leo-Aqua@users.noreply.github.com>
2024-03-26 08:22:18 +0100

    renamed:    IServAPI.py -> IServAPI/IServAPI.py modified:   pyproject.toml modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="IServAPI",
-    version="0.1.0",
+    version="0.1.0a",
     author="Leo Aqua",
     author_email="contact@leoaqua.de",
     description="Unofficial API for IServ",
