6c46f7fe9e2025b81875fb07cb01dd4de5af6fc3
Leo-Aqua <g00g7el@gmail.com>
2025-11-27 22:37:57 +0100

    Bump version to 1.3.1 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="IServAPI",
-    version="1.3.0",
+    version="1.3.1",
     author="Leo Aqua",
     author_email="contact@leoaqua.de",
     description="Unofficial API for IServ",
